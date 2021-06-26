ebf6d173fc755e7aeeef468e0c6a46bebb9a61d1
Tyler Manning <tylerericmanning@gmail.com>
2021-06-26 19:40:32 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Tauri+React is now running successfully!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
